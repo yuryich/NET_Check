6a542e2f841ec772034fb4cae32243a0625cc259
Neto test <yury120405@gmail.com>
2024-05-28 11:52:53 +0400

    Initial commit

---
+++ NET_Check/NET_Check.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Check
+{
+	int number_check;
+	string name;
+	double balance;
+};
+
+void new_balance(Check &acount, int new_balanc) {
+	acount.balance = new_balanc;
+}
+
+
+int main()
+{
+	Check acount;
+	int n;
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите номер счёта: ";
+	cin >> acount.number_check;
+	cout << "Введите имя владельца: ";
+	cin >> acount.name;
+	cout << "Введите баланс: ";
+	cin >> acount.balance;
+	cout << "Введите новый баланс: ";
+	cin >> n;
+	new_balance(acount, n);
+	cout << "Ваш счёт: " << acount.name << ", " << acount.number_check << ", " << acount.balance;
+}
+    
